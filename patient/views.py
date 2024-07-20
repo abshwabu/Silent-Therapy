3d5ac3a1a6d52591adb016de535b2a913096a298
ashwabu <abdulazizredi90@gmail.com>
2024-07-20 15:33:17 +0300

    next page

--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from backend.models import Patient
+from . import serializers
+
+class PatientViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.PatientSerializer
+    queryset = Patient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
