83b08ae14b73bc886ee604b985e2fece43b1347d
ashwabu <abdulazizredi90@gmail.com>
2024-07-20 09:12:42 +0300

    user model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
 from django.db import models
-
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 # Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, username, password=None, **extra_fields):
+        """Create a new user."""
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, password):
+        """Create a new superuser."""
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+
