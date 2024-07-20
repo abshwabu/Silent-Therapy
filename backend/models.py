a5cfdd2f93840e9187c557cc4fba2adbb83908d9
ashwabu <abdulazizredi90@gmail.com>
2024-07-20 11:09:19 +0300

    registration and login api

--- backend/models.py
+++ backend/models.py
@@ -4,17 +4,20 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
+    Group,
+    Permission,
 )
-# Create your models here.
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, username, password=None, **extra_fields):
         """Create a new user."""
+        if not username:
+            raise ValueError('The Username field must be set')
         user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, username, password):
@@ -23,18 +26,33 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
-
         return user
-
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
 
+# Update settings.py to use your custom user model
+# AUTH_USER_MODEL = 'your_app_name.User'
--- silent_therapy/settings.py
+++ silent_therapy/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'backend',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True
+}
